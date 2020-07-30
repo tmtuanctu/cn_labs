a384e42b89baf4c5c1994bee87129baca8e0c073
tunmgcc18076 <68980789+tunmgcc18076@users.noreply.github.com>
2020-07-30 10:36:52 +0700

    Update lab03_02.py

--- lab03_02.py
+++ lab03_02.py
@@ -6,9 +6,7 @@ from mininet.node import CPULimitedHost, Host, Node
 from mininet.node import OVSKernelSwitch, UserSwitch
 from mininet.node import IVSSwitch
 from mininet.cli import CLI
-from mininet.log import setLogLevel, info
-from mininet.link import TCLink, Intf
-from subprocess import call
+call
 
 def myNetwork():
 
